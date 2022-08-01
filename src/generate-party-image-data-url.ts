b461a8c68c6dd9b85f3b36683847d07ed75b5a1c
Gabriel Chiconi <gabriel@lins.dev>
2022-08-01 17:02:45 -0300

    add faint bg to image

--- src/generate-party-image-data-url.ts
+++ src/generate-party-image-data-url.ts
@@ -11,11 +11,15 @@ export const generatePartyImage = async (pokemon: Pokemon[]) => {
   const ctx = canvas.getContext("2d");
   ctx.imageSmoothingEnabled = false;
 
+  ctx.fillStyle = "#ffffff60";
+  ctx.fillRect(0, 0, 360, 80);
+
   const pokemonUrls = pokemon.map((mon) => serebiiUrl(mon.pokedexNumber));
   const images = await Promise.all(pokemonUrls.map(loadImage));
   images.forEach((img, i) => ctx.drawImage(img, i * 60, 0));
 
   ctx.font = '16px "PKMN RBYGSC"';
+  ctx.fillStyle = "#000";
   pokemon.forEach(({ level }, i) =>
     ctx.fillText(`lv${level}`, i * 60 + 15, 70, 30)
   );
